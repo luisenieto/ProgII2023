eb89251dc0663c6e91658767d0b1b13133e28de4
Luis Nieto <lnieto@herrera.unt.edu.ar>
2023-05-03 12:17:16 -0300

    Tercer commit

---
+++ src/unPaquete/Cliente.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package unPaquete;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author root
+ */
+public class Cliente {
+    String correo;
+    String clave;
+    String apellido;
+    String nombre;    
+    
+    void mostrar() {
+        System.out.println(correo);
+        System.out.println(apellido);
+        System.out.println(nombre);
+    }
+    
+}
